9299b879b8320e525a8f23c076a5b26de2fc6148
Katie Edmonds Derkach <katie.edmonds1829@gmail.com>
2023-10-11 14:56:11 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// Generate random month 
+
+const randMonth = () => {
+    const monthNum = Math.floor(Math.random() * 12) + 1;
+    
+    let month = '';
+    
+    switch (monthNum) {
+        case 1:
+            month = 'January';
+            break;
+        case 2:
+            month = 'February';
+            break;
+        case 3:
+            month = 'March';
+            break;
+        case 4:
+            month = 'April';
+            break;
+        case 5:
+            month = 'May';
+            break;
+        case 6:
+            month = 'June';
+            break;
+        case 7:
+            month = 'July';
+            break;
+        case 8:
+            month = 'August';
+            break;
+        case 9:
+            month = 'September';
+            break;
+        case 10:
+            month = 'October';
+            break;
+        case 11:
+            month = 'November';
+            break;
+        case 12:
+            month = 'December'
+            break;
+    }
+    console.log(month);
+};
+
+randMonth()
+
+// Generate a random country
+
